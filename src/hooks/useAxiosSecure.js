d4be1db1735ba3385618abc53c89e8a37d1c34c0
sami157 <sami.eee157@gmail.com>
2026-02-12 18:06:27 +0600

    hardcoded server URL for testing

--- src/hooks/useAxiosSecure.js
+++ src/hooks/useAxiosSecure.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-    baseURL: import.meta.env.VITE_serverUrl,
+    baseURL: 'https://dining-management-server.vercel.app'
 });
 
 console.log(axiosSecure.baseURL);
