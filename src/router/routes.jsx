b60453db5244d51c015d3f7dcda8c97b03069e2e
sami157 <sami.eee157@gmail.com>
2026-02-17 19:09:12 +0600

    added cname and hydrate fallback

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -17,7 +17,7 @@ export const router = createBrowserRouter([
     {
         path: "/",
         Component: HomeLayout,
-        hydrateFallbackElement: Loading,
+        hydrateFallbackElement: <Loading/>,
         children: [
             {
                 index: true,
